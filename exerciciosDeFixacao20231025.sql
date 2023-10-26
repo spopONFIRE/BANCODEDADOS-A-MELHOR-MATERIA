6d1a81197905a7bde2aff0110c31d9c8d95f3520
spopONFIRE <116584135+spopONFIRE@users.noreply.github.com>
2023-10-25 23:17:35 -0300

    Exercicio 1

--- exerciciosDeFixacao20231025.sql
+++ exerciciosDeFixacao20231025.sql
@@ -1,1 +1,10 @@
-
+--Exercicio 1
+DELIMITER //
+CREATE TRIGGER after_insert_clientes
+AFTER INSERT ON Clientes
+FOR EACH ROW
+BEGIN
+    INSERT INTO Auditoria (...) VALUES (CONCAT('Novo cliente', NOW()));
+END;
+//
+DELIMITER ;
